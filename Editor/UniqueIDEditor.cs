a728e0b72a52c468e95528760606891489b562cf
vaibhavdevstuff <vaibhav-jaiswal@outlook.com>
2024-08-24 19:41:39 +0530

    Added Namespace

--- Editor/UniqueIDEditor.cs
+++ Editor/UniqueIDEditor.cs
@@ -1,34 +1,38 @@
 using UnityEditor;
 using UnityEngine;
+using DC;
 
-[CustomEditor(typeof(UniqueID))]
-public class UniqueIDEditor : Editor
+namespace DCEditor
 {
-    public override void OnInspectorGUI()
+    [CustomEditor(typeof(UniqueID))]
+    public class UniqueIDEditor : Editor
     {
-        // Draw the default inspector (so you keep the rest of the fields)
-        DrawDefaultInspector();
-
-        // Cast the target to UniqueID type
-        UniqueID uniqueIDScript = (UniqueID)target;
-
-        EditorGUILayout.Space();
-
-        // If there is no unique ID, show "Generate" button
-        if (string.IsNullOrEmpty(uniqueIDScript.ID))
+        public override void OnInspectorGUI()
         {
-            if (GUILayout.Button("Generate"))
+            // Draw the default inspector (so you keep the rest of the fields)
+            DrawDefaultInspector();
+
+            // Cast the target to UniqueID type
+            UniqueID uniqueIDScript = (UniqueID)target;
+
+            EditorGUILayout.Space();
+
+            // If there is no unique ID, show "Generate" button
+            if (string.IsNullOrEmpty(uniqueIDScript.ID))
             {
-                uniqueIDScript.GenerateUniqueID();
+                if (GUILayout.Button("Generate"))
+                {
+                    uniqueIDScript.GenerateUniqueID();
+                }
             }
-        }
-        else
-        {
-            // If a unique ID exists, show "Regenerate" button
-            if (GUILayout.Button("Re-generate"))
+            else
             {
-                uniqueIDScript.ClearUniqueID();
-                uniqueIDScript.GenerateUniqueID();
+                // If a unique ID exists, show "Regenerate" button
+                if (GUILayout.Button("Re-generate"))
+                {
+                    uniqueIDScript.ClearUniqueID();
+                    uniqueIDScript.GenerateUniqueID();
+                }
             }
         }
     }
